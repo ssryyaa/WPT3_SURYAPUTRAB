a884fe90f5a2135d6b22e0022ab8fbe7969688b5
ssryyaa <145754874+ssryyaa@users.noreply.github.com>
2023-10-27 14:39:39 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Surya</li> 
+        <li>Nama Belakang : Putra</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Mojosongo Jebres Surakarta </li> 
+    </ul> 
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Surakarta</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>bersepeda</li> 
+        <li>Lari</li> 
+    </ul> 
+</section>
